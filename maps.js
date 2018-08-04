1d1dd02c8de0cc85af498289607b7d9776c206a5
shriram <shriramvenugopal123@gmail.com>
2018-08-04 15:53:48 +0530

    created map interface

---
+++ maps.js
@@ -0,0 +1,31 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2hyaXJhbTk4IiwiYSI6ImNqa2Y0NTk0bjAzdGMzcnM3enNsNmM1d24ifQ.QVOrH27sDYsWSZVpLvr6fg';
+var map = L.map('map').setView([9.9312, 76.2673], 14);
+var i = 0;
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+map.on('click', function(e) {
+    new L.marker([e.latlng.lat, e.latlng.lng]).addTo(map)
+    .bindPopup('location ' + i)
+    .on('mouseover', function(e){
+        this.openPopup();
+    })
+    .on('mouseout', function(e) {
+        this.closePopup();
+    });
+
+    i++;
+
+});
+
+new L.marker([10.028413, 76.328717]).addTo(map)
+    .bindPopup('model engineering college')
+    .on('mouseover', function(e){
+        this.openPopup();
+    })
+    .on('mouseout', function(e) {
+        this.closePopup();
+    });
+
